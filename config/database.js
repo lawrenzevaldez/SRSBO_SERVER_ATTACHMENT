381a4fca00f32a5ccccb4bb2ba977200338d1edd
lawrenzevaldez <valdezlawrenze@gmail.com>
2026-03-04 16:43:34 +0800

    REMOVE CHANGE SUPPLIER BUTTON AND INCREASE DATABASE TIMEOUT

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
+const Env = use("Env");
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
-const Helpers = use('Helpers')
+const Helpers = use("Helpers");
 
 module.exports = {
   /*
@@ -16,7 +16,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get("DB_CONNECTION", "sqlite"),
 
   /*
   |--------------------------------------------------------------------------
@@ -30,12 +30,14 @@ module.exports = {
   |
   */
   sqlite: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(
+        `${Env.get("DB_DATABASE", "development")}.sqlite`,
+      ),
     },
     useNullAsDefault: true,
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false),
   },
 
   /*
@@ -49,37 +51,45 @@ module.exports = {
   |
   */
   mysql: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', 'srsnova'),
-      database: Env.get('DB_DATABASE', 'returned_merchandise')
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", "srsnova"),
+      database: Env.get("DB_DATABASE", "returned_merchandise"),
     },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false),
   },
   transfers: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', 'srsnova'),
-      database: Env.get('DB_DATABASE_TRANSFERS', 'transfers')
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", "srsnova"),
+      database: Env.get("DB_DATABASE_TRANSFERS", "transfers"),
     },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get("DB_DEBUG", false),
   },
   srspos: {
-    client: 'mssql',
+    client: "mssql",
     connection: {
-      host: Env.get('DB_HOST_MS', '192.168.0.148'),
-      port: Env.get('DB_PORT_MS', ''),
-      user: Env.get('DB_USER_MS', 'markuser'),
-      password: Env.get('DB_PASSWORD_MS', 'tseug'),
-      database: Env.get('DB_DATABASE_SRSPOS', 'NOVA_JADE')
+      host: Env.get("DB_HOST_MS", "192.168.0.148"),
+      port: Env.get("DB_PORT_MS", ""),
+      user: Env.get("DB_USER_MS", "markuser"),
+      password: Env.get("DB_PASSWORD_MS", "tseug"),
+      database: Env.get("DB_DATABASE_SRSPOS", "NOVA_JADE"),
+      connectTimeout: 150000,
     },
-    debug: Env.get('DB_DEBUG', false)
+    pool: {
+      min: 0,
+      max: 10,
+      acquireTimeoutMillis: 150000,
+      // ⏱ Close idle connections
+      idleTimeoutMillis: 30000,
+    },
+    debug: Env.get("DB_DEBUG", false),
   },
 
   /*
@@ -93,14 +103,14 @@ module.exports = {
   |
   */
   pg: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", ""),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", ""),
+      database: Env.get("DB_DATABASE", "adonis"),
     },
-    debug: Env.get('DB_DEBUG', false)
-  }
-}
+    debug: Env.get("DB_DEBUG", false),
+  },
+};
